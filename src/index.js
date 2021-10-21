410d8262d29d329927301ea28330946a7d467a46
陳宜榛 <37102145+Jane0901@users.noreply.github.com>
2021-10-21 20:17:31 +0800

    set font and add theme, GlobalStyle

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import "./index.css"
-import App from "./App"
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import Normalize from 'react-normalize'
+import { GlobalStyle } from './styles/GlobalStyle'
+import { ThemeProvider } from 'styled-components'
+import theme from './styles/theme'
 
-ReactDOM.render(<App />, document.getElementById("root"))
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <Normalize />
+    <GlobalStyle />
+    <App />
+  </ThemeProvider>,
+  document.getElementById('root')
+)
---
+++ src/styles/theme.js
@@ -0,0 +1,25 @@
+const theme = {
+  // primary
+  primary_100: '#FFF3B3',
+  primary_200: '#FFD803',
+  primary_300: '#B8A53A',
+
+  // secondary
+  secondary: '#272343',
+
+  // danger
+  danger: '#E56051',
+
+  // general
+  general_000: '#FFFFFF',
+  general_100: '#F9F9F9',
+  general_200: '#EAEBED',
+  general_300: '#D3D4D6',
+  general_400: '#CFCFCF',
+  general_500: '#B7B7B7',
+  general_600: '#727272',
+  general_700: '#474747',
+  general_900: '#000000',
+}
+
+export default theme
