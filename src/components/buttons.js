4da82f7e7e6bfcdf52a5a84094f1e4067dd8b688
陳宜榛 <37102145+Jane0901@users.noreply.github.com>
2021-10-27 23:14:18 +0800

    revise ButtonSmall and add ButtonSuperLarge

--- src/components/buttons.js
+++ src/components/buttons.js
@@ -4,6 +4,7 @@ import styled from 'styled-components'
 /* button/btn_small */
 const SmallButton = styled.button`
   display: flex;
+  justify-content: center;
   align-items: center;
   text-align: center;
   width: 93px;
@@ -21,6 +22,23 @@ const SmallButton = styled.button`
 
   /* brand/secondary/purple */
   color: ${(props) => props.theme.secondary};
+
+  :hover {
+    background: ${(props) => props.theme.primary_100};
+  }
 `
+
+const SuperLargeButton = styled(SmallButton)`
+  width: 100%;
+  height: 44px;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 24px;
+  letter-spacing: 0.5px;
+`
+
 // label：寫按鈕上面的文字
 export const ButtonSmall = ({ label }) => <SmallButton>{label}</SmallButton>
+export const ButtonSuperLarge = ({ label }) => (
+  <SuperLargeButton>{label}</SuperLargeButton>
+)
