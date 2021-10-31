b093d4ba3fd37d0d07a2a97b7fc714c905b15706
Genie <forgenie4all@gmail.com>
2021-11-01 05:23:48 +0800

    finish HomePage layout

--- src/components/buttons.js
+++ src/components/buttons.js
@@ -24,3 +24,25 @@ const SmallButton = styled.button`
 `
 // label：寫按鈕上面的文字
 export const ButtonSmall = ({ label }) => <SmallButton>{label}</SmallButton>
+
+/* button/btn_small */
+const MediumButton = styled.button`
+  display: flex;
+  align-items: center;
+  text-align: center;
+  width: 160px;
+  height: 51px;
+  padding: 12px 36px;
+  font-size: 18px;
+  line-height: 27px;
+  letter-spacing: 0.5px;
+  text-transform: uppercase;
+  background: ${(props) => props.theme.primary_200};
+  border-radius: 4px;
+  border: none;
+
+  /* brand/secondary/purple */
+  color: ${(props) => props.theme.secondary};
+`
+// label：寫按鈕上面的文字
+export const ButtonMedium = ({ label }) => <MediumButton>{label}</MediumButton>
