afa37a095fdd455c8ba56c7ee98c291345b402c7
陳宜榛 <37102145+Jane0901@users.noreply.github.com>
2021-11-01 13:09:52 +0800

    modify router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import FrontNavbar from './components/Navbar/FrontNavbar'
 import Footer from './components/Footer/Footer'
+import BackstageNavbar from './components/Navbar/BackstageNavbar'
 import { HashRouter as Router, Switch, Route } from 'react-router-dom'
-import styled from 'styled-components'
 import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
@@ -24,101 +24,54 @@ import ManageCategoryPage from './pages/ManageCategoryPage'
 import ManageFaqPage from './pages/ManageFaqPage'
 import ManageGivingPage from './pages/ManageGivingPage'
 
-const PageContainer = styled.div`
-  padding-top: 64px;
-`
+const Home = () => {
+  return (
+    <>
+      <FrontNavbar />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/login" component={LoginPage} />
+        <Route path="/register" component={RegisterPage} />
+        <Route path="/about" component={AboutPage} />
+        <Route exact path="/givings" component={GivingsPage} />
+        <Route path="/givings/add" component={AddGiftPage} />
+        <Route path="/givings/edit" component={EditGiftsPage} />
+        <Route path="/givings/:id" component={ItemPage} />
+        <Route exact path="/portfolio" component={PortfolioPage} />
+        <Route path="/portfolio/edit" component={EditPortfolioPage} />
+        <Route exact path="/transactions" component={TransactionsPage} />
+        <Route path="/transactions/detail" component={TransactionsDetailPage} />
+        <Route path="/faq" component={FaqPage} />
+        <Route path="/privacy" component={PrivacyPage} />
+        <Route path="/terms" component={TermsPage} />
+      </Switch>
+      <Footer />
+    </>
+  )
+}
+
+const Backstage = () => {
+  return (
+    <>
+      <BackstageNavbar />
+      <Switch>
+        <Route exact path="/backstage" component={BackstagePage} />
+        <Route path="/backstage/member" component={ManageMemberPage} />
+        <Route path="/backstage/category" component={ManageCategoryPage} />
+        <Route path="/backstage/faq" component={ManageFaqPage} />
+        <Route path="/backstage/giving" component={ManageGivingPage} />
+      </Switch>
+    </>
+  )
+}
 
 export default function App() {
   return (
     <Router>
-      <PageContainer>
-        <Switch>
-          <Route exact path="/">
-            <FrontNavbar />
-            <HomePage />
-            <Footer />
-          </Route>
-          <Route path="/login">
-            <FrontNavbar />
-            <LoginPage />
-          </Route>
-          <Route path="/register">
-            <FrontNavbar />
-            <RegisterPage />
-          </Route>
-          <Route path="/about">
-            <FrontNavbar />
-            <AboutPage />
-            <Footer />
-          </Route>
-          <Route path="/givings">
-            <FrontNavbar />
-            <GivingsPage />
-            <Footer />
-          </Route>
-          <Route exact path="/givings/:id">
-            <FrontNavbar />
-            <ItemPage />
-            <Footer />
-          </Route>
-          <Route path="/givings/add">
-            <FrontNavbar />
-            <AddGiftPage />
-            <Footer />
-          </Route>
-          <Route path="/givings/edit">
-            <FrontNavbar />
-            <EditGiftsPage />
-            <Footer />
-          </Route>
-          <Route path="/portfolio">
-            <FrontNavbar />
-            <PortfolioPage />
-          </Route>
-          <Route path="/portfolio/edit">
-            <FrontNavbar />
-            <EditPortfolioPage />
-          </Route>
-          <Route path="/transactions">
-            <FrontNavbar />
-            <TransactionsPage />
-          </Route>
-          <Route path="/transactions/detail">
-            <FrontNavbar />
-            <TransactionsDetailPage />
-          </Route>
-          <Route path="/faq">
-            <FrontNavbar />
-            <FaqPage />
-            <Footer />
-          </Route>
-          <Route path="/privacy">
-            <FrontNavbar />
-            <PrivacyPage />
-            <Footer />
-          </Route>
-          <Route path="/terms">
-            <FrontNavbar />
-            <TermsPage />
-            <Footer />
-          </Route>
-          <Route path="/backstage">
-            <BackstagePage />
-          </Route>
-          <Route path="/backstage/member">
-            <ManageMemberPage />
-          </Route>
-          <Route path="/backstage/category">
-            <ManageCategoryPage />
-          </Route>
-          <Route path="/backstage/faq">
-            <ManageFaqPage />
-          </Route>
-          <Route path="/backstage/giving">
-            <ManageGivingPage />
-          </Route>
-        </Switch>
-      </PageContainer>
+      <Switch>
+        <Route path="/backstage" component={Backstage} />
+        <Route path="/" component={Home} />
+      </Switch>
     </Router>
   )
 }
