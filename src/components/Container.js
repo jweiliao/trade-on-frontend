e35752d121aaeee0d5863eccb8ee63426fe21c80
陳宜榛 <37102145+Jane0901@users.noreply.github.com>
2021-11-09 18:11:48 +0800

    modify Container setting

--- src/components/Container.js
+++ src/components/Container.js
@@ -5,7 +5,7 @@ const Container = styled.div`
   max-width: 1040px;
   margin: 3rem auto;
   ${MEDIA_QUERY_MAX_WIDTH} {
-    padding: 0 22px;
+    padding: 0 2.5rem;
   }
 `
 export default Container
