11466496c5b67cdfa9cecb187da9104bd99f05d8
陳宜榛 <37102145+Jane0901@users.noreply.github.com>
2021-11-10 14:13:00 +0800

    add BackstageTitle

--- src/components/heading.js
+++ src/components/heading.js
@@ -1,8 +1,16 @@
 import styled from 'styled-components'
+import { MEDIA_QUERY_SM } from '../styles/breakpoints'
 
 export const PageTitle = styled.h2`
   text-align: center;
   font-size: 1.63rem;
+`
+
+export const BackstageTitle = styled(PageTitle)`
+  margin: 3rem 0 2rem;
+  ${MEDIA_QUERY_SM} {
+    margin-top: 16rem;
+  }
 `
 
 export const SubTitle = styled.h3`
