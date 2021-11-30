34eaede169baaf9c8e5a9bc51cfc2b86194b39f1
陳宜榛 <37102145+Jane0901@users.noreply.github.com>
2021-11-30 15:43:00 +0800

    fix rwd, fetch post

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,210 +1,147 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
-import { MEDIA_QUERY_SM, MEDIA_QUERY_MD } from '../../styles/breakpoints'
+import { MEDIA_QUERY_MD } from '../../styles/breakpoints'
 import { MediumButton } from '../../components/buttons'
 import Container from '../../components/Container'
-
-// 首頁最上方大圖
 import homebanner from '../../images/homeBanner.svg'
-// 首頁中間的禮物圖示
-import gift from '../../images/homeGift.svg'
-// 首頁關於我們的圖片
 import homeintro from '../../images/homeIntro.svg'
-
-// 引入 Carousel.js
 import Carousel from '../../components/Carousel/Carousel'
 
-/* 首頁上方 banner */
-const HomeBanner = styled.div`
+const Banner = styled.div`
   width: 100%;
-  margin-top: 20px;
   display: flex;
   justify-content: center;
   align-items: center;
-
-  ${MEDIA_QUERY_SM} {
-    margin-top: 50px;
-    flex-direction: column;
+  margin-bottom: 7rem;
+  ${MEDIA_QUERY_MD} {
+    flex-direction: column-reverse;
+    margin-bottom: 5rem;
   }
 `
 
-/* 首頁 banner - 左側整個文字元件 */
-const BannerText = styled.div`
-  padding-right: 50px;
+const BannerTextWrapper = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-
-  ${MEDIA_QUERY_SM} {
-    margin-bottom: 50px;
-    padding-right: 0px;
+  margin-right: 3rem;
+  flex: 1;
+  ${MEDIA_QUERY_MD} {
+    margin: 3rem 0 0;
   }
 `
 
-/* 首頁 banner - 左側文字標題 */
-const BannerTitle = styled.div`
-  font-size: 72px;
-  font-weight: 700;
-  line-height: 108px;
+const Title = styled.h1`
+  font-size: 3.5rem;
+  margin: 0 0 2rem;
+  letter-spacing: 0.2rem;
+  color: ${(props) => props.theme.primary_200};
+  ${MEDIA_QUERY_MD} {
+    display: none;
+  }
+`
+
+const SubTitle = styled.h2`
+  font-weight: 550;
+  line-height: 1.5;
   letter-spacing: 0.5px;
   color: ${(props) => props.theme.primary_200};
+`
 
+const BannerImgWrapper = styled.div`
+  flex: 2;
+  width: 100%;
   ${MEDIA_QUERY_MD} {
-    margin-bottom: 20px;
+    max-width: 44rem;
+    flex: 1;
   }
 `
 
-/* 首頁 banner - 左側文字副標題 */
-const BannerSubTitle = styled.div`
-  padding-top: 5px;
-  font-size: 25px;
-  font-weight: 700;
-  line-height: 48px;
-  letter-spacing: 2px;
-  color: ${(props) => props.theme.primary_200}; ;
+const BannerImage = styled.img`
+  width: 100%;
 `
 
-/* 首頁 banner - 右側圖片 */
-const BannerImage = styled.img``
-
-/* 首頁中間 "我想送禮物" - 整個元件 */
-const HomeGift = styled.div`
-  height: 500px;
-  margin-top: 30px;
+const HomeRecommended = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-`
-/* 首頁中間 "我想送禮物" - 圖片 */
-const GiftImage = styled.img``
-
-/* 首頁中間 "我想送禮物" - 文字的連接 */
-const GiftTitle = styled(Link)`
-  margin-top: 20px;
-  font-weight: 500;
-  font-size: 24px;
-  line-height: 36px;
-  color: ${(props) => props.theme.secondary};
-
-  &:hover {
-    color: ${(props) => props.theme.primary_200};
-  }
+  margin-bottom: 10rem;
 `
 
-/* 首頁中間的介紹 - 整個元件 */
-const HomeIntro = styled.div`
-  padding: 30px; 37px;
+const Intro = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-
-  ${MEDIA_QUERY_SM} {
+  margin-bottom: 1rem;
+  ${MEDIA_QUERY_MD} {
     flex-direction: column;
   }
 `
 
-/* 首頁中間的介紹 - 左邊整個元件 */
-const HomeIntroLeft = styled.div`
-  margin-left: 55px;
-  width: 427px;
-  height: 427px;
-`
-/* 首頁中間的介紹 - 左邊的圖片 */
-const IntroImage = styled.img``
-
-/* 首頁中間的介紹 - 右邊整個元件 */
-const HomeIntrolRight = styled.div`
-  margin-left: 72px;
-  width: 430px;
-  height: 430px;
-
-  ${MEDIA_QUERY_SM} {
-    margin-top: 50px;
+const IntroImgWrapper = styled.div`
+  margin-right: 4rem;
+  ${MEDIA_QUERY_MD} {
+    margin: 0 0 3rem;
   }
 `
-/* 首頁中間的介紹 - 右邊文字標題 */
-const IntroTitle = styled.div`
-  font-weight: 500;
-  font-size: 26px;
-  line-height: 39px;
+
+const IntroImage = styled.img`
+  width: 100%;
+`
+
+const IntroTextWrapper = styled.div``
+
+const IntroTitle = styled(SubTitle)``
+
+const IntroContent = styled.p`
+  margin: 2rem 0;
+  font-size: 1.125rem;
+  line-height: 1.5;
   letter-spacing: 0.5px;
 `
 
-/* 首頁中間的介紹 - 右邊文字內容 */
-const IntroContent = styled.div`
-  margin-top: 50px;
-  margin-bottom: 50px;
-  padding-right: 10px;
-  font-size: 20px;
-  line-height: 28px;
-  letter-spacing: 0.5px;
-`
-
-/* 首頁下方的物品推薦  */
-const HomeRecommended = styled.div`
-  padding: 72px 50px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
+const GivingPageButton = styled(MediumButton)`
+  ${MEDIA_QUERY_MD} {
+    width: 100%;
+    max-width: 25rem;
+    margin: 0 auto;
+  }
 `
 
 export default function HomePage() {
   return (
-    <>
-      {/* 首頁最上方的 banner 區塊 */}
-      <HomeBanner>
-        {/* banner 左側的文字 */}
-        <BannerText>
-          <BannerTitle>Trade On</BannerTitle>
-          <BannerSubTitle>美好的事物值得繼續傳遞</BannerSubTitle>
-        </BannerText>
-        {/* banner 右側的圖片 */}
-        <BannerImage src={homebanner} />
-      </HomeBanner>
-      <Container>
-        {/* 首頁中間的 "我想送禮物" 區塊 */}
-        <HomeGift>
-          {/* "我想送禮物" 上方圖片*/}
-          <Link to="/givings">
-            <GiftImage src={gift} />
-          </Link>
-          {/* "我想送禮物" 下方文字*/}
-          <GiftTitle to="/givings">我想送禮物</GiftTitle>
-        </HomeGift>
-
-        {/* 首頁中間的 "介紹" 區塊*/}
-        <HomeIntro>
-          {/* "介紹" 左側的圖片 */}
-          <HomeIntroLeft>
-            <IntroImage src={homeintro} />
-          </HomeIntroLeft>
-
-          {/* "介紹" 右側的文字 */}
-          <HomeIntrolRight>
-            <IntroTitle>想斷捨離，卻不知如何處理物品嗎？</IntroTitle>
-            <IntroContent>
-              <p>我們相信，每個曾經陪伴過的物品，都有值得傳遞的故事與價值</p>
-              <br />
-              <p>有時，你只是在尋找下一個懂得珍惜它的人。</p>
-            </IntroContent>
-
-            {/* "介紹"右側文字下方的按鈕 */}
-            <Link to="/about">
-              <MediumButton>了解更多</MediumButton>
-            </Link>
-          </HomeIntrolRight>
-        </HomeIntro>
-
-        {/* 首頁最下方的推薦物品區塊 */}
-        <HomeRecommended>
-          {/* 引入 Carousel.js 的 components */}
-          <Carousel></Carousel>
-        </HomeRecommended>
-      </Container>
-    </>
+    <Container>
+      <Banner>
+        <BannerTextWrapper>
+          <Title>Trade On</Title>
+          <SubTitle>美好的事物值得繼續傳遞</SubTitle>
+        </BannerTextWrapper>
+        <BannerImgWrapper>
+          <BannerImage src={homebanner} />
+        </BannerImgWrapper>
+      </Banner>
+      <HomeRecommended>
+        <Carousel></Carousel>
+      </HomeRecommended>
+      <Intro>
+        <IntroImgWrapper>
+          <IntroImage src={homeintro} />
+        </IntroImgWrapper>
+        <IntroTextWrapper>
+          <IntroTitle>想斷捨離，卻不知如何處理物品嗎？</IntroTitle>
+          <IntroContent>
+            我們相信，每一個曾經陪伴過的物品，都有值得延續的故事與價值。
+            <br />
+            <br />
+            有時，你只是在尋找下一個懂得珍惜它的人。
+          </IntroContent>
+          <GivingPageButton as={Link} to="/givings">
+            送禮物
+          </GivingPageButton>
+        </IntroTextWrapper>
+      </Intro>
+    </Container>
   )
 }
