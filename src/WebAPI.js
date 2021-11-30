507b119e0a8396bcd21ec80f10afa004fc827692
陳宜榛 <37102145+Jane0901@users.noreply.github.com>
2021-11-30 15:40:22 +0800

    add post API

--- src/WebAPI.js
+++ src/WebAPI.js
@@ -1,33 +1,40 @@
 import axios from 'axios'
 const config = {
   apiHost1: 'http://localhost:8081',
-  apiHost2: 'http://184.73.187.145:5000',
+  apiHost2: 'https:/cosdelus.tw/tradeon/api',
 }
 
 export const instance = axios.create({
   baseURL: config.apiHost2,
 })
 
-// 常見問題相關
+// post
+export const getAllPosts = (limit) => instance.get(`/posts/all?size=${limit}`)
 
-// 取得常見問題
+export const getPost = (id) => instance.get(`/posts/${id}`)
+
+export const addPost = (data) => instance.post(`/posts/new`, data)
+
+export const updatePost = (id, data) => instance.post(`/posts/${id}`, data)
+
+export const deletePost = (id) => instance.delete(`/posts/${id}`)
+
+export const getLimitPost = (page, limit, owner, isPublic) =>
+  instance.get(
+    `/posts/all?page=${page}&size=${limit}&user=${owner}&isPublic=${isPublic}`
+  )
+
+// faq
 export const getAllFaqs = (limit) =>
   instance.get(`/commonqnas/all?size=${limit}`)
 
-// 取得特定一筆常見問題
 export const getFaq = (id) => instance.get(`/commonqnas/${id}`)
 
-// 新增常見問題
 export const addFaq = (data) => instance.post('/commonqnas/new', data)
 
-// 編輯常見問題
 export const updateFaq = (id, data) => instance.put(`/commonqnas/${id}`, data)
 
-// 刪除常見問題
 export const deleteFaq = (id) => instance.delete(`/commonqnas/${id}`)
 
-// 根據參數 page、limit 拿到限定第幾頁且每頁多少篇的文章
 export const getLimitFaq = (page, limit) =>
   instance.get(`/commonqnas/all?page=${page}&size=${limit}`)
-
-// export default instance
