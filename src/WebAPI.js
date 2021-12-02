d1ab6a5d88bc7c7f349730781f70e4050d995f2d
陳宜榛 <37102145+Jane0901@users.noreply.github.com>
2021-12-03 01:02:57 +0800

    add user API

--- src/WebAPI.js
+++ src/WebAPI.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+
 const config = {
   apiHost1: 'http://localhost:8081',
   apiHost2: 'https:/cosdelus.tw/tradeon/api',
@@ -6,7 +7,24 @@ const config = {
 
 export const instance = axios.create({
   baseURL: config.apiHost2,
+  headers: { withCredentials: true },
 })
+
+// user
+export const register = async (email, nickname, password, confirmPassword) =>
+  await instance.post('/users/register', {
+    email,
+    nickname,
+    password,
+    confirmPassword,
+  })
+
+export const login = async (email, password) =>
+  await instance.post('/users/login', { email, password })
+
+export const getMe = async () => await instance.get(`/users/me`)
+
+export const logout = async () => await instance.get('/users/logout')
 
 // post
 export const getAllPosts = (limit) => instance.get(`/posts/all?size=${limit}`)
