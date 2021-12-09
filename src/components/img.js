c97a12bd97f1a0865b5008ad14c6a4c69c5f64fc
陳宜榛 <37102145+Jane0901@users.noreply.github.com>
2021-12-09 14:47:32 +0800

    add component img

---
+++ src/components/img.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components'
+
+export const Img = styled.img`
+  max-height: 100%;
+  max-width: 100%;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  margin: auto;
+`
+
+export const ImgWrapper = styled.div`
+  position: relative;
+  border: solid 1px ${(props) => props.theme.general_200};
+`
+
+export const ImgCircleWrapper = styled(ImgWrapper)`
+  border-radius: 50%;
+  overflow: hidden;
+`
