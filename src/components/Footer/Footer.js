e8d9d2d0e9bfae00a1bbd2a6a4e8a254022213e9
wade <aaa51782@gmail.com>
2022-01-25 17:12:45 +0800

    footer done

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,13 +5,9 @@ import { MEDIA_QUERY_SM, MEDIA_QUERY_MD } from '../../styles/breakpoints'
 
 const FooterRow = styled.footer`
   background-color: ${(props) => props.theme.general_400};
-  padding: 50px 0 20px 0;
+  padding: 15px 0 15px 0;
 `
 const FooterArea = styled.div`
-  display: flex;
-  justify-content: space-between;
-  flex-direction: row;
-  flex-wrap: wrap;
   max-width: 1040px;
   margin: 0 auto;
   ${MEDIA_QUERY_MD} {
@@ -19,18 +15,15 @@ const FooterArea = styled.div`
   }
 `
 const FooterUl = styled.ul`
-  ${MEDIA_QUERY_SM} {
-    width: 50%;
-  }
+  display: flex;
+  justify-content: center;
+  flex-direction: row;
+  flex-wrap: wrap;
 `
 const FooterLi = styled.li`
   align-items: center;
-  padding: 5px 0;
+  padding: 5px;
   font-size: 16px;
-  &:first-child {
-    font-weight: 500;
-    font-size: 18px;
-  }
 `
 const FooterLink = styled(Link)`
   color: ${(props) => props.theme.secondary};
@@ -39,28 +32,23 @@ const FooterLink = styled(Link)`
     color: ${(props) => props.theme.general_700};
   }
 `
+const HyperLink = styled.a`
+  color: ${(props) => props.theme.secondary};
+  font-weight: normal;
+  &:hover {
+    color: ${(props) => props.theme.general_700};
+  }
+`
+
 const Copyright = styled.div`
   text-align: center;
-  margin-top: 40px;
+  margin-top: 5px;
 `
 
 export const Footer = () => {
   return (
     <FooterRow>
       <FooterArea>
-        <FooterUl>
-          {FooterData.about.map((item, index) => {
-            return (
-              <FooterLi key={index}>
-                {item.link ? (
-                  <FooterLink to={item.link}>{item.title}</FooterLink>
-                ) : (
-                  item.title
-                )}
-              </FooterLi>
-            )
-          })}
-        </FooterUl>
         <FooterUl>
           {FooterData.help.map((item, index) => {
             return (
@@ -79,13 +67,14 @@ export const Footer = () => {
             return (
               <FooterLi key={index}>
                 {item.link ? (
-                  <FooterLink
+                  <HyperLink
                     to={item.link}
                     target="_blank"
                     rel="noreferrer noopener"
+                    href={item.link}
                   >
                     {item.title}
-                  </FooterLink>
+                  </HyperLink>
                 ) : (
                   item.title
                 )}
@@ -94,7 +83,7 @@ export const Footer = () => {
           })}
         </FooterUl>
       </FooterArea>
-      <Copyright>&copy; 2021 TRADE ON All rights reserved.</Copyright>
+      <Copyright>&copy; 2022 TRADE ON All rights reserved.</Copyright>
     </FooterRow>
   )
 }
